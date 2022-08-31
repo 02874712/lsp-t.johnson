239448af6df01e4ea761a983746831af125f8831
Tashiana <tashiana.johnson@bison.howard.edu>
2022-08-31 19:11:36 -0400

    This is my commit for assignment #1.

---
+++ lsp-t.johnson/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Tashiana Johnson
+ * 
+ * @author Tashiana
+ *
+ *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
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO AND WELCOME TO LSP!");
+	}
+
+}
