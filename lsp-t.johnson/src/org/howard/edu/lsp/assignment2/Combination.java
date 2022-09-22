771c96d0cf8f4e80fdb6eb1731efdf17abc544f0
Tashiana <tashiana.johnson@bison.howard.edu>
2022-09-22 17:40:45 -0400

    Updating Combination Class...

--- lsp-t.johnson/src/org/howard/edu/lsp/assignment2/Combination.java
+++ lsp-t.johnson/src/org/howard/edu/lsp/assignment2/Combination.java
@@ -1,9 +1,22 @@
 package org.howard.edu.lsp.assignment2;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+ * September 22, 2022
+ * @02874712
+ * @Tashiana Johnson
+ */
 
 public class Combination {
-
+	/**
+	 * 
+	 * @param array - set of primitive integers
+	 * @param total_sum - int sum 
+	 * @return
+	 */
 	public ArrayList<String> calculateCombinations(int[] array, int total_sum){
+
 		ArrayList<String> combo_arr = new ArrayList<String>();
 		
 		for(int i = 0; i < array.length; i++) {
@@ -12,12 +25,18 @@ public class Combination {
 			}
 			for(int j = 1; j < array.length; j++) {
 				if(array[i] + array[j] == total_sum && i != j) {
-					combo_arr.add("[" + i + "," + j + "]");
-				}
+					if(array[j] + array[i] == total_sum)
+						{System.out.print("Duplicate! ");}
+					else {
+						combo_arr.add("[" + i + "," + j + "]");
+						}
+		
+					}
 			}
 		}
 		return combo_arr;
-	}
 
+	}
+	
 }
 																																																																														
