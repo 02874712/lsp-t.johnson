953f587d603a9cdc691c67a21bba35a653440054
Tashiana <tashiana.johnson@bison.howard.edu>
2022-10-10 18:26:43 -0400

    Javadocs created for class IntegerSet

--- lsp-t.johnson/src/org/howard/edu/lsp/assignment4/implementation/IntegerSet.java
+++ lsp-t.johnson/src/org/howard/edu/lsp/assignment4/implementation/IntegerSet.java
@@ -1,23 +1,43 @@
-/**
- * 
- */
 package org.howard.edu.lsp.assignment4.implementation;
 import java.util.ArrayList;
+
 /**
  * @author Tashiana
+ * Class IntegerSet Works Over an ArrayList Object that provides basic mathematics set functionality
  *
  */
 public class IntegerSet {
 	
+	/**
+	 * Creating private variables that are only to be accessible by the calling object.
+	 * @set - ArrayList of Integers || this.set()
+	 * @largest - largest num in set, stored within this.set()
+	 * @smallest - smallest num in set, stored within this.set() - initialized to a set number
+	 */
 	  private ArrayList<Integer> set = new ArrayList<Integer>();
 	  private int largest , smallest = 100; 
 
+	  /**
+	   * Default Constructor - IntegerSet is Created
+	   */
 	  public IntegerSet(){};
-
+	  
+	  /**
+	   * clear() - void function removing all integers of ArrayList set
+	   */
 	  public void clear() {set.clear();};
 
-	  public int length() {return set.size();}; // returns the length
-
+	  /**
+	   * length() - calculates the given length of private variable set
+	   * @return length of ArrayList set
+	   */
+	  public int length() {return set.size();}; 
+	  
+	  /**
+	   * equals(b) - compares the values of this.set() to declared IntegerSet b 
+	   * @param b - IntegerSet object(not of this.set()) to be compared  
+	   * @return isListEqual_False - given an initialized false value ; boolean returns true if b values == this.set values
+	   */
 	  public boolean equals(IntegerSet b) {
 	    Boolean isListEqual_False = false; 
 	    if ( set.size() != b.length() ){
@@ -35,6 +55,11 @@ public class IntegerSet {
 	    return isListEqual_False; 
 	  }; 
 
+	  /**
+	   * contains(value) - searches through the given obj for param value
+	   * @param value - int passed to function, if found listContains_false is set to true
+	   * @return listContains_false - given an initialized false value; boolean returns true if this.set contains the given value
+	   */
 	  public boolean contains(int value) {
 	    Boolean listContains_false = false; 
 	    if(set.contains(value) == true){
@@ -44,7 +69,11 @@ public class IntegerSet {
 	    return listContains_false;
 	  };    
 
-	// Returns the largest item in the set; Throws a IntegerSetException if the set is empty 
+	/**
+	 * largest - searches for the largest number in a given ArrayList set 
+	 * @return largest - integer found to the the max of this.set
+	 * @throws IntegerSetException - Exception Thrown when/if this.set is NULL/EMPTY
+	 */
 	public int largest() throws IntegerSetException{
 	  
 	  try{
@@ -64,8 +93,12 @@ public class IntegerSet {
 	  return largest; 
 	}; 
 
-	// Returns the smallest item in the set; Throws a IntegerSetException if the set is empty
-	public int smallest() throws IntegerSetException {
+	/**
+	 * smallest - searches for the min number in a given ArrayList set 
+	 * @return smallest - integer found to the the min of this.set
+	 * @throws IntegerSetException - Exception Thrown when/if this.set is NULL/EMPTY
+	 */
+		public int smallest() throws IntegerSetException {
 	  try{
 	    if(set.isEmpty()){
 	      throw new IntegerSetException("Set Empty Exception Thrown!");
@@ -82,8 +115,10 @@ public class IntegerSet {
 
 	  return smallest; 
 	};
-
-	// Adds an item to the set or does nothing it already there	
+		/**
+		 * add(item) - adds an item to the set and does nothing it set contains item
+		 * @param item - value to be added to the given set
+		 */
 	 	public void add(int item){
 	    if(set.contains(item) == true){
 	      //System.out.printf("%i is already in the list!\n", item);
@@ -91,30 +126,42 @@ public class IntegerSet {
 	    else{
 	      set.add(item);
 	    }
-	  }; // adds item to the set or does nothing if it is in set
+	  };
 
-	// Removes an item from the set or does nothing if not there
+	/**
+	 * remove(item) - removes an item from the given set and does nothing if its not in set
+	 * @param item - value to be removed from a given set
+	 */
 	public void remove(int item) {
 	  if(set.contains(item) == true){
 	    set.remove(set.indexOf(item));
 	    }
 	  }
 
-	// Set union
+	/**
+	 * union(IntegerSet) - combines two IntegerSet objects such that no repeated values are added
+	 * @param intSetb - to be the second set that is combined with this.set()
+	 */
 	public void union(IntegerSet intSetb) {
 	  for(int i = 0; i < set.size(); i++) {
 	    intSetb.add(set.get(i)); 
 	  }
 	};
 
-	// Set intersection
+	/**
+	 * intersect(IntegerSet) - gives back a ArrayList set of values that can be found in both IntegerSets
+	 * @param intSetb - second set that is intersected with this.set()
+	 */
 	public void intersect(IntegerSet intSetb) {
 	  for(int i = 0; i < set.size(); i++){
 	    set.retainAll(intSetb.set);
 	    }
 	  }; 
 
-	// Set difference, i.e., s1 –s2
+	/**
+	 * diff(IntegerSet) - gives back the difference of intSetb from this.set(), returning values only found in this.set()
+	 * @param intSetb - given IntegerSet ArrayList that is compared against this.set()
+	 */
 	public void diff(IntegerSet intSetb){
 	  for(int i = 0; i < set.size(); i++){
 	    if (intSetb.contains(set.get(i))){
@@ -122,13 +169,19 @@ public class IntegerSet {
 	      i--;
 	    }
 	  }
-	}; // set difference, i.e. s1 - s2
+	};
 
-	// Returns true if the set is empty, false otherwise
+	/**
+	 * isEmpty - checks to find if the given set is empty
+	 * @return boolean value true if empty, else false if it is not NULL
+	 */
 	public boolean isEmpty(){ return set.isEmpty(); }; 
 
-	// Return String representation of your set
-	public String toString() { return set.toString(); };	// return String representation of your set
+	/**
+	 * toString - turns ArrayList values to String
+	 * @return String representation of this.set() at time of call
+	 */
+	public String toString() { return set.toString(); };
 
 
 }
--- lsp-t.johnson/src/org/howard/edu/lsp/assignment4/test/Driver.java
+++ lsp-t.johnson/src/org/howard/edu/lsp/assignment4/test/Driver.java
@@ -4,10 +4,13 @@
 package org.howard.edu.lsp.assignment4.test;
 import org.howard.edu.lsp.assignment4.implementation.IntegerSet;
 import org.howard.edu.lsp.assignment4.implementation.IntegerSetException;
+
 /**
  * @author Tashiana
+ * Assignment #4
  *
  */
+
 public class Driver {
 
 	/**
@@ -60,6 +63,7 @@ public class Driver {
 	    System.out.printf("Finding the Max of Set 3: %d\n", testSet3.largest());
 	    System.out.printf("Finding the Max of Set 2: %d\n", testSet2.largest());
 	    System.out.printf("Finding the Min of Set 4: %d\n", testSet4.smallest());
+	    
 	    System.out.println("Let's Remove Value 5 from Set 1...");
 	    testSet1.remove(5);
 
@@ -71,7 +75,7 @@ public class Driver {
 	    System.out.printf("Is Set 1 Equal to Set 2? %s\n", testSet1.equals(testSet2));
 
 	    System.out.printf("Is Set 3 Empty? %s\n", testSet3.isEmpty());
-	    System.out.println("Let's union Set 2 and Set 3...");
+	    System.out.println("Let's perform Set 2 Union Set 3...");
 	    testSet2.union(testSet3);
 	    System.out.printf("Set 3: %s\n", testSet3.toString());
 	    System.out.printf("Is Set 3 Empty? %s\n", testSet3.isEmpty());
@@ -80,15 +84,15 @@ public class Driver {
 	    System.out.printf("Set Intersection of 1 and 4: %s\n", testSet1.toString() );
 
 	    testSet4.union(testSet3);
-	    System.out.printf("Set Union of 3 and 4: %s\n", testSet3.toString());
+	    System.out.printf("Set 3 Union Set 4: %s\n", testSet3.toString());
 
 	    System.out.printf("Set 2: %s\n", testSet2.toString());
 	    System.out.printf("Set 4: %s\n", testSet4.toString());
-	    testSet4.diff(testSet2);
 	    
+	    testSet4.diff(testSet2);
 	    System.out.printf("Set Difference of 2 and 4: %s\n", testSet4.toString());
 
-	    System.out.printf("Junk Collection || Clearing Data...\nOur new Sets are...\n"); 
+	    System.out.printf("Junk Collection || Clearing Sets 1, 2, and 4...\nOur new Sets are...\n"); 
 	    testSet1.clear();
 	    testSet2.clear();
 	    testSet4.clear();
